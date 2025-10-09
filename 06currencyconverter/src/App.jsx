c94cfc9f8cc9cd6e306724fc3171baafdd61787d
Jay <jaygondaliya2309@gmail.com>
2025-10-10 00:59:55 +0530

    currency converter project half

---
+++ 06currencyconverter/src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+      }}
+    >
+      <h1 className="bg-red-300">Jay file</h1>
+    </div>
+  );
+}
+
+export default App;
